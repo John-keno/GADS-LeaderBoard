0f51641615c584531f7861614fdf72cf3a16d106
John Keno <iwehjohn@gmail.com>
2020-09-11 21:33:45 +0100

    update initial commit

--- app/src/main/java/com/github/johnkeno/gadsleadersboard/network/main/APIPostService.java
+++ app/src/main/java/com/github/johnkeno/gadsleadersboard/network/main/APIPostService.java
@@ -8,11 +8,11 @@ import retrofit2.http.POST;
 public interface APIPostService {
 
     @FormUrlEncoded
-    @POST("1FAIpQLSdjIIDDnQlRcDMjkNu-Pr8o9tWMaeVoP95YMnSpVPrACagucQ/formResponse")
+    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
     Call<PostData> submitData(
-            @Field("entry.27885089") String firstName,
-            @Field("entry.542538513") String LastName,
-            @Field("entry.17301063") String email,
-            @Field("entry.924667838") String GitHubLink
+            @Field("entry.1877115667") String firstName,
+            @Field("entry.2006916086") String LastName,
+            @Field("entry.1824927963") String email,
+            @Field("entry.284483984") String GitHubLink
     );
 }
